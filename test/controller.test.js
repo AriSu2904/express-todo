9e7f0e224e663d1a2ab5b4ed6e2f9c3e71241901
AriSu2904 <arisusanto290401@gmail.com>
2023-10-05 01:36:15 +0700

    Testing - Get Todo

--- test/controller.test.js
+++ test/controller.test.js
@@ -9,5 +9,13 @@ beforeEach(async () => {
 })
 
 afterEach(async () => {
-    await mongoose.close();
+    await mongoose.connection.close();
 })
+
+describe("GET /api/v1/todolist", () => {
+    it("should return todolist", async () => {
+        const res = await request(app).get('/api/v1/todolist');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toBeGreaterThan(0);
+    });
+});
