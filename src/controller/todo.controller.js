7f57617530b887d48d5fe2c5144cdb0c5286f62e
AriSu2904 <arisusanto290401@gmail.com>
2023-10-05 00:42:08 +0700

    Controller - Get All

---
+++ src/controller/todo.controller.js
@@ -0,0 +1,12 @@
+const TodoList = require('../model/todo.model');
+const httpStatus = require('http-status-codes');
+
+class TodoController {
+    getAll = (req, res) => {
+        TodoList.find().then((todos) => {
+            return res.status(httpStatus.OK).send(todos);
+        }).catch((err) => {
+            return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err);
+        })
+    }
+}
